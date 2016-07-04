1c9b2ac241b31b73008ed39b0898f72faa89a574
Jens Stegemann <jwstegemann@gmail.com>
2016-07-04 17:12:01 +0200

    new test for component

--- src/main/scala/proceed/diff/patch/Patch.scala
+++ src/main/scala/proceed/diff/patch/Patch.scala
@@ -11,31 +11,31 @@ sealed trait Patch {
 
 case class CreateNewChild(parent: Element, child: Element, sibbling: Option[Node]) extends Patch {
   def apply() = {
-
+    println("create new Element " + child + " @ " + parent + " before " + sibbling)
   }
 }
 
 case class DeleteChild(parent: Element, child: Element) extends Patch {
   def apply() = {
-
+    println("delete Element " + child + " @ " + parent)
   }
 }
 
 case class RemoveAttribute(element: Element, attribute: String) extends Patch {
   def apply() = {
-
+    println("  remove Attribute " + attribute + " @ " + element)
   }
 }
 
 case class SetAttribute(element: Element, attribute: String, value: String) extends Patch {
   def apply() = {
-
+    println("  set Attribute " + attribute + "=" + value +  " @ " + element)
   }
 }
 
 case class MoveChild(parent: Element, child: Element, sibbling: Option[Node]) extends Patch {
   def apply() = {
-
+    println("move Element " + child + " @ " + parent + " before " + sibbling)
   }
 }
 
