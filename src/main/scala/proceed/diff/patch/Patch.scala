f6f2bb8b7eba9c76c947c2ab80e287c9a8c1670e
Jan Weidenhaupt <jan.weidenhaupt@gmx.de>
2016-07-16 13:21:22 +0200

    change id to data-proceed

--- src/main/scala/proceed/diff/patch/Patch.scala
+++ src/main/scala/proceed/diff/patch/Patch.scala
@@ -54,7 +54,7 @@ case class CreateNewChild(parent: Element, child: Element, sibbling: Option[Node
       case textNode: TextNode => textNode.domRef = Some(Right(dom.document.createTextNode(textNode.content)))
       case element: Element => {
         val newDomElement = dom.document.createElement(child.nodeType)
-        newDomElement.setAttribute("id",child.childrensPath)
+        newDomElement.setAttribute("data-proceed",child.childrensPath)
         element.domRef = Some(Left(newDomElement))
       }
     }
